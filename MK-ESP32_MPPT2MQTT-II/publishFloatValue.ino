8e11edbd325f1a4245ca3faa90ef0d65cf05f9b8
MK4001 <50479511+mk4001@users.noreply.github.com>
2024-01-26 10:33:26 +0100

    Update publishFloatValue.ino

--- MK-ESP32_MPPT2MQTT-II/publishFloatValue.ino
+++ MK-ESP32_MPPT2MQTT-II/publishFloatValue.ino
@@ -1,3 +1,9 @@
+/*
+  © MK 15 Jan 2024
+  Write Victron Energy MPPT fields to MQTT
+  based on ESP32 Wroom using Uses VeDirectFrameHandler library
+*/
+
 void publishFloatValue(const char* topic, float value) {
   char message[20];
   snprintf(message, sizeof(message), "%.2f", value);
