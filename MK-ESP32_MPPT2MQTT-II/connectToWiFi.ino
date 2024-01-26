edc7e31102faa8b8961298b3c7b44409906efceb
MK4001 <50479511+mk4001@users.noreply.github.com>
2024-01-26 10:33:14 +0100

    Update connectToWiFi.ino

--- MK-ESP32_MPPT2MQTT-II/connectToWiFi.ino
+++ MK-ESP32_MPPT2MQTT-II/connectToWiFi.ino
@@ -1,3 +1,9 @@
+/*
+  © MK 15 Jan 2024
+  Write Victron Energy MPPT fields to MQTT
+  based on ESP32 Wroom using Uses VeDirectFrameHandler library
+*/
+
 void connectToWiFi() {
   Serial.print("Connecting to WiFi...");
   WiFi.begin(ssid, password);
